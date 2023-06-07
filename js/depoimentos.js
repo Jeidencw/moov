79bd3146c89d29f3565254b1d5b45040a697d52d
Jeidencw <henriquecw22@gmail.com>
2023-06-06 22:36:43 -0300

    slides depoimentos

---
+++ js/depoimentos.js
@@ -0,0 +1,41 @@
+const swiper = new Swiper('.slide__content', {
+    loop: true,
+    slidesPerView: 3,
+    spaceBetween: 30,
+    slidesPerGroup: 3,
+    centerSlide: 'true',
+    fade: 'true',
+    grabCursor: 'true',
+    keyboard: {
+        enabled: true,
+    },
+    pagination: {
+      el: '.swiper-pagination',
+      clickable: true,
+    },
+    navigation: {
+      nextEl: '.swiper-button-next',
+      prevEl: '.swiper-button-prev',
+    },
+    autoplay: {
+        delay: 3000,
+        disableOnInteraction: false,
+    },
+
+    breakpoints: {
+        0:{
+            slidesPerView: 1,
+            slidesPerGroup: 1,
+            autoplay: {
+                delay: 2000,
+            },
+        },
+        520:{
+            slidesPerView: 2,
+            slidesPerGroup: 2, 
+        },
+        950:{
+            slidesPerView: 3, 
+        },
+    },
+});
