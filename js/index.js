c159595ab0d16f06250c44b3c06da0ea6d2ba33e
Jeidencw <henriquecw22@gmail.com>
2023-09-18 22:50:32 -0300

    upgrades

--- js/index.js
+++ js/index.js
@@ -2,22 +2,33 @@
 //mostra menu
 const openMenu = document.getElementById("openMenu");
 const closeMenu = document.getElementById("closeMenu");
-const menu = document.getElementById("menu")
+const menu = document.getElementById("menu");
+const menuItems = document.querySelectorAll('.nav__link');
 
 openMenu.addEventListener("click", () => {
-    menu.classList.add("show-menu")
+    menu.style.display = 'block'
+    menu.classList.add('menuOpen')
 })
 
 closeMenu.addEventListener("click", () => {
-    menu.classList.remove("show-menu")
+    menu.style.display = 'none'
+    menu.classList.remove('menuOpen')
 })
 
+menuItems.forEach((menuItem => {
+    menuItem.addEventListener('click', () => {
+        if(menu.classList.contains('menuOpen')){
+            menu.style.display = 'none'
+        }
+    })
+}))
 
 //mostra modal
 const modal = document.getElementsByClassName("modal")
 const saibaMais = document.getElementsByClassName("saibaMais")
 const fechaModal = document.getElementsByClassName("fechaModal")
 const card = document.getElementsByClassName("card")
+const servicesContent = document.getElementsByClassName("services__content")
 
 
 for(let i = 0; i < saibaMais.length; i++){
@@ -25,10 +36,16 @@ for(let i = 0; i < saibaMais.length; i++){
         modal[i].classList.add("active-modal")
     })
 
+    modal[i].addEventListener('click', event => {
+        if(event.target === modal[i]){
+            modal[i].classList.remove('active-modal')
+        }
+    })
+
     fechaModal[i].addEventListener("click", () => {
         modal[i].classList.remove("active-modal")
     })
-
+    
     card[i].addEventListener("click", () => {
         modal[i].classList.add("active-modal")
     })
